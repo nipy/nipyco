e937e308f2c89ca912430be3f9a6b030ead7c49b
Jarrod Millman <jarrod.millman@gmail.com>
2009-08-28 19:59:33 +0000

    cleaning up
    git-svn-id: https://nipy.svn.sourceforge.net/svnroot/nipy/devel/trunk@295 ead46cd0-7350-4e37-8683-fc4c6f79bf00

--- conf.py
+++ conf.py
@@ -31,7 +31,29 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'sphinx.ext.pngmath',
               'sphinx.ext.ifconfig',
-              'ipython_console_highlighting']
+              'ipython_console_highlighting',
+              'inheritance_diagram',
+              'numpydoc']
+
+# Matplotlib sphinx extensions
+# ----------------------------
+
+# Currently we depend on some matplotlib extentions that are only in
+# the trunk, so we've added copies of these files to fall back on,
+# since most people install releases.  Once theses extensions have
+# been released for a while we should remove this hack.  I'm assuming
+# any modifications to these extensions will be done upstream in
+# matplotlib!  The matplotlib trunk will have more bug fixes and
+# feature updates so we'll try to use that one first.
+try:
+    import matplotlib.sphinxext
+    extensions.append('matplotlib.sphinxext.mathmpl')
+    extensions.append('matplotlib.sphinxext.only_directives')
+    extensions.append('matplotlib.sphinxext.plot_directive')
+except ImportError:
+    extensions.append('mathmpl')
+    extensions.append('only_directives')
+    extensions.append('plot_directive')
 
 
 # Add any paths that contain templates here, relative to this directory.
